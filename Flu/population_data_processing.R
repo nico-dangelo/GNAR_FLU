a6e3cbcab437f934b2553c16b4db49517065accf
Nico D'Angelo <nd672@mac.mynetworksettings.com>
2026-03-20 14:47:46 -0400

    Added preprocessing for county-level population that includes legacy CT FIPS and HHS5

--- Flu/population_data_processing.R
+++ Flu/population_data_processing.R
@@ -9,6 +9,9 @@ co_est2020_alldata_clean <- co_est2020_alldata |> filter(SUMLEV!="040")
 # check dropping
 identical(co_est2020_alldata |> filter(COUNTY!="000"), co_est2020_alldata_clean)
 
+#add concatenated fips
+co_est2020_alldata_clean<- co_est2020_alldata_clean |> mutate(FIPS=str_pad(paste0(STATE, COUNTY),5,pad="0"))
+co_est2020_alldata_clean<- co_est2020_alldata_clean |> select(c("SUMLEV" ,"REGION", "DIVISION","STATE","COUNTY", "FIPS", everything()))
 # Subsets -----------------------------------------------------------------
 
 # 10k
@@ -29,4 +32,6 @@ co_est2020_NENG_10k <- co_est2020_alldata_clean_10k |> filter(STATE  %in% NENG_s
 saveRDS(co_est2020_NENG_10k, file="~/Library/CloudStorage/GoogleDrive-nd672@georgetown.edu/My Drive/Lab Files/GNAR_FLU/Data/Population/co_est2020_NENG_10k.RDS")
 co_est2020_NENG_20k <- co_est2020_alldata_clean_20k |> filter(STATE %in% NENG_states)
 saveRDS(co_est2020_NENG_20k, file="~/Library/CloudStorage/GoogleDrive-nd672@georgetown.edu/My Drive/Lab Files/GNAR_FLU/Data/Population/co_est2020_NENG_20k.RDS")
-HHS_5_states <- 
+HHS_5_states <- state_fips_lookup(c("IL", "IN", "MI", "MN", "OH", "WI"))
+co_est2020_HHS_5_10k <- co_est2020_alldata_clean_10k |> filter(STATE  %in% HHS_5_states)
+saveRDS(co_est2020_HHS_5_10k, file="~/Library/CloudStorage/GoogleDrive-nd672@georgetown.edu/My Drive/Lab Files/GNAR_FLU/Data/Population/co_est2020_HHS_5_10k.RDS")
