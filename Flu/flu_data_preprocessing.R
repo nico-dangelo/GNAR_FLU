06455018db7c8e85ba13d37d80e44c36459a815a
Nico D'Angelo <nd672@mac.mynetworksettings.com>
2026-03-23 13:43:07 -0400

    Added season fit flag to drop rows outside November to March

--- Flu/flu_data_preprocessing.R
+++ Flu/flu_data_preprocessing.R
@@ -65,7 +65,18 @@ county_flu_ac_season_merged <- merge.data.frame(county_flu_ac_season, county_sea
 
 county_flu_ac_season_norm <- county_flu_ac_season_merged %>% mutate(conf_flu_norm = conf_flu_adj /
                                                                       season_all_cause_wtd)
-# saveRDS(county_flu_ac_season_norm, "~/Library/CloudStorage/GoogleDrive-nd672@georgetown.edu/My Drive/Lab Files/GNAR_FLU/Data/Flu/county_flu_ac_season_norm.RDS")
+
+# Add range flag for limited fits
+county_flu_ac_season_norm <- county_flu_ac_season_norm |> mutate(season_fit = ifelse(
+  month(year_week_dt) >= 11,
+  paste0(year(year_week_dt), "-", year(year_week_dt) + 1),      # Nov-Dec: "2023-2024"
+  ifelse(
+    month(year_week_dt) <= 3,
+    paste0(year(year_week_dt) - 1, "-", year(year_week_dt)),    # Jan-Mar: "2023-2024"
+    NA_character_
+  )
+))
+saveRDS(county_flu_ac_season_norm, "~/Library/CloudStorage/GoogleDrive-nd672@georgetown.edu/My Drive/Lab Files/GNAR_FLU/Data/Flu/county_flu_ac_season_norm.RDS")
 rm(county_flu_ac_season, county_flu_ac_season_merged, county_season_ac_v3_imputed, county_week_ac_norm, county_week_ac_v3_imputed, county_week_ac_v3_imputed_year,county_week_flu_ac_merged, county_week_flu_ac_merged_adj, county_week_flu_v3_imputed, county_week_flu_v3_imputed_clean, county_year_ac_mean)
 # Import and Process Shapefile --------------------------------------------
 
