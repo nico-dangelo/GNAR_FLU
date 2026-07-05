f2550d61ff37309e3de099e643d2372f5fa34098
Nico D'Angelo <81781015+nico-dangelo@users.noreply.github.com>
2026-07-05 19:04:24 -0400

    Run updated diagnostics on singular model fits

--- Flu/GNAR_metapop_synthetic.R
+++ Flu/GNAR_metapop_synthetic.R
@@ -53,13 +53,37 @@ all(colnames(real_pop_norm_ts_aligned) == net_counties)                      # o
 
 real_pop_GNAR_fit <- GNARfit(vts = real_pop_norm_ts_aligned, net = mobility_weighted_GNAR_Nov, alphaOrder = 1, betaOrder = c(1))
 #Singular
+
+# Diagnostics on residuals and covariance matrix --------------------------
+
+perform_gnar_residual_checks(real_pop_GNAR_fit)
+test_residual_normality(real_pop_GNAR_fit, method = "shapiro")|>  select(Result) |> table()
+test_residual_normality(real_pop_GNAR_fit, method="lilliefors") |>  select(Result)|> table()
+test_residual_heteroscedasticity(real_pop_GNAR_fit)|> select(Homoscedastic) |> table()
+check_residual_diagnostics(real_pop_GNAR_fit) |> summarize_diagnostics()
+check_residual_diagnostics(real_pop_GNAR_fit) |>  filter(Lilliefors_Result == "Non-normal",
+                                                                                Shapiro_Result == "Normal") %>%
+                                                                        arrange(desc(Prop_Ties)) %>% head(10)
+# plot_residuals_vs_fitted(real_pop_GNAR_fit)
+diagnose_vcov_matrix(real_pop_GNAR_fit)|> print_vcov_diagnostics()
+plot_vcov_diagnostics(diagnose_vcov_matrix(real_pop_GNAR_fit))
+corbit_plot(vts=real_pop_norm_ts_aligned, net=mobility_weighted_GNAR_Nov)
+# Try sqrt tranform
+real_pop_norm_ts_aligned_sqrt <- sqrt( real_pop_norm_ts_aligned)
+real_pop_GNAR_sqrt_fit <- GNARfit(vts = real_pop_norm_ts_aligned_sqrt, net = mobility_weighted_GNAR_Nov, alphaOrder = 1, betaOrder = c(1))
+summary(real_pop_GNAR_sqrt_fit)
+perform_gnar_residual_checks(real_pop_GNAR_sqrt_fit)
+test_residual_normality(real_pop_GNAR_sqrt_fit)
+test_residual_heteroscedasticity(real_pop_GNAR_sqrt_fit)
+check_residual_diagnostics(real_pop_GNAR_sqrt_fit)|> summarize_diagnostics()
 # Uniform county populations
 infected_series_uniform_pop <- read_csv("Data/Flu/infected_series_uniform_pop.csv")
-infected_series_uniform_pop_burden <- infected_series_real_pop |> mutate(county_pop=1E5, flu_burden=100*I/county_pop)
+infected_series_uniform_pop_burden <- infected_series_real_pop |> mutate(county_pop=1E5, flu_burden=I/county_pop)
 infected_series_uniform_pop_ts <- infected_series_uniform_pop_burden |> select(time, county, flu_burden) |> spread(county, flu_burden) |> column_to_rownames(var="time")|> as.matrix()
 infected_series_uniform_pop_ts_aligned <- infected_series_uniform_pop_ts[, net_counties]
 uniform_pop_GNAR_fit <- GNARfit(vts=infected_series_uniform_pop_ts_aligned, net=mobility_weighted_GNAR_Nov)
 summary(uniform_pop_GNAR_fit)
+perform_gnar_residual_checks(uniform_pop_GNAR_fit)
 #Singular
 
 # Synthetic fits with unweighted networks ---------------------------------
@@ -68,3 +92,20 @@ uniform_pop_unweighted_GNAR_fit <- GNARfit(vts=infected_series_uniform_pop_ts_al
 real_pop_unweighted_GNAR_fit <- GNARfit(vts=real_pop_norm_ts_aligned, net=mobility_unweighted_GNAR_Nov)
 summary(uniform_pop_unweighted_GNAR_fit)
 summary(real_pop_unweighted_GNAR_fit)
+perform_gnar_residual_checks(real_pop_unweighted_GNAR_fit)
+perform_gnar_residual_checks(uniform_pop_unweighted_GNAR_fit)
+
+# Fits with Mean-centered, standardized data --------------------------------------------
+# ts.plot(abs(infected_series_uniform_pop_ts-mean(infected_series_uniform_pop_ts))/sd(infected_series_uniform_pop_ts))
+infected_series_uniform_pop_ts_aligned_centered<- (infected_series_uniform_pop_ts_aligned- colMeans(infected_series_uniform_pop_ts_aligned))
+infected_series_uniform_pop_ts_aligned_centered_fit<- GNARfit(vts=infected_series_uniform_pop_ts_aligned_centered, net=mobility_unweighted_GNAR_Nov)
+ce
+infected_series_uniform_pop_ts_aligned_standard <- infected_series_uniform_pop_ts_aligned_centered/apply(infected_series_uniform_pop_ts_aligned,2,function(X){sd(X,na.rm = T)})
+ts.plot(infected_series_uniform_pop_ts_aligned_standard)
+infected_series_uniform_pop_ts_aligned_standard_fit <- GNARfit(vts=infected_series_uniform_pop_ts_aligned_standard, net=mobility_unweighted_GNAR_Nov, alphaOrder = 1, betaOrder = 1)
+
+
+
+# Jittered covariance for BIC computation ---------------------------------
+
+
